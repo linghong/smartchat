0a7c7d47f24472b9a75c77ba01aebf0c5e44440e
linghong <linghong_chen@hotmail.com>
2023-06-19 17:08:09 -0400

    add ChatMessage component.

---
+++ src/components/ChatMessage.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import { Message } from '@/src/types/chat';
+
+type ChatMessageProps = {
+  message: Message
+  lastIndex: boolean, 
+  loading: boolean
+}
+
+const ChatMessage: React.FC<ChatMessageProps>  = ({message, lastIndex, loading}) => {
+
+  return (
+    <>
+      {message.question.length !== 0 && 
+        <div 
+          className="flex p-3 text-black"
+        >
+          <Image
+            src="/user.png"
+            alt="User"
+            width="30"
+            height="30"
+            className="h-full mr-4 rounded-sm"
+            priority
+          />
+          <p>{message.question}</p>
+        </div>}
+      {<div 
+        className="flex p-3 bg-slate-100 color-black ease-in duration-300"
+      >
+        <Image
+          src="/bot.png"
+          alt="AI Bot"
+          width="30"
+          height="30"
+          className={`h-full mr-4 rounded-sm ${loading && lastIndex &&'animate-pulse'}`}
+          priority
+        />
+        <p>{message.answer}</p>     
+      </div>}
+    </>
+  );
+}
+
+export default ChatMessage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
-import {useState, useRef } from 'react';
-import ArrowButton  from '@/src/components/ArrowButton';
+import {useState, useRef } from 'react'
+import ArrowButton  from '@/src/components/ArrowButton'
+import ChatMessage from '@/src/components/ChatMessage'
+import { Message } from '@/src/types/chat'
 
 const HomePage = () => {
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
-  const [userInput, setUserInput] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
+  const [userInput, setUserInput] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+  /*const [chatHistory, setChatHistory] = useState<Message[]>([
+    {
+      question: '', 
+      answer:'Hi, How can I assist you?'
+    }])*/
 
   const handleSubmit = () => {
 
   }
+
+  const chatHistory = [ {
+    question: '', 
+    answer:'Hi, How can I assist you?'
+  }, {
+    question: 'Hello', 
+    answer:'Hi, How can I assist you?'
+  }]
 
   return  (
     <div className="flex flex-col">
@@ -17,21 +33,35 @@ const HomePage = () => {
         Chat With AI
       </h1>
       <div className="flex flex-col items-center justify-between">
-        <form onSubmit={handleSubmit} className="flex item-center w-4/5 my-4 p-2 border-2 border-indigo-200 rounded-lg hover:bg-indigo-100 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-red">
-            <textarea
-              ref={textAreaRef}
-              disabled={loading}
-              autoFocus={false}
-              rows={1}
-              maxLength={512}
-              id="userInput"
-              name="userInput"
-              placeholder="Click to send a message, and click shift + enter to go to the next line."
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-              className={`placeholder-gray-100 focus: p-2 ${loading ? 'opacity-50' : ''} w-80vw focus:ring-stone-100 focus:outline-none`}
-            />
-            <ArrowButton disabled={userInput===''} />
+        <div className={`w-80vw h-80vh bg-white border-2 border-indigo-100 rounded-lg`}>
+          <div className="w-full h-full overflow-y-scroll rounded-lg">
+            {chatHistory.map((chat, index) => 
+              <ChatMessage
+                key={index}
+                message={chat}
+                lastIndex={index===chatHistory.length-1?true:false}
+                loading={loading}
+              />)}
+          </div>
+        </div>
+        <form 
+          onSubmit={handleSubmit} 
+          className="flex item-center w-80vw my-4 p-2 border-2 border-indigo-300 rounded-lg hover:bg-indigo-100 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-red"
+        >
+          <textarea
+            ref={textAreaRef}
+            disabled={loading}
+            autoFocus={false}
+            rows={1}
+            maxLength={512}
+            id="userInput"
+            name="userInput"
+            placeholder="Click to send a message, and click shift + enter to go to the next line."
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            className={`w-80vw placeholder-gray-400 focus: p-2 ${loading && 'opacity-50'}focus:ring-stone-100 focus:outline-none`}
+          />
+          <ArrowButton disabled={userInput===''} />
         </form>
       </div>
     </div> 
