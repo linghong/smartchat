614a59db6b3507b8d4e5db1e635354844e2f5aae
linghong <linghong_chen@hotmail.com>
2023-07-02 12:37:16 -0400

    Now the API can be used to send the user's question to OpenAI to fetch an answer.

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getChatResponse } from '@/src/services/openai'
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,5 +15,17 @@ export default async function handler(
   if (!question) {
     return res.status(400).json({ message: 'No question in the request' })
   }
-  console.log(question, nameSpace, history)
+  // replacing newlines with spaces
+  const sanitizedQuestion = question.trim().replaceAll('\n', ' ')
+
+  try {
+    const chatResponse = await getChatResponse(sanitizedQuestion)
+    const chatAnswer = chatResponse?.content || 'I am sorry. I can\'t find an answer to your question.'
+    console.log('AI response', chatAnswer);
+    res.status(200).json(chatAnswer);
+  
+  } catch (error: any) {
+    console.log('error', error);
+    res.status(500).json({ error: error.message || 'Something went wrong' });
+  }
 }
--- src/services/openai.ts
+++ src/services/openai.ts
@@ -15,6 +15,29 @@ export const createEmbedding = async (text: string) => {
   return embedding.data.data[0].embedding;
 }
 
+export const getChatResponse = async (userMessage : string) => {
+  try {
+    const chatCompletion = await openaiClinet.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0,
+        max_tokens: 2048,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        top_p: 1,
+        messages: [
+          {role: "system", 
+          content: "You are an AI assistant and an expert. You should use both the information provided by the user and your pre-existing knowledge to answer the questions."
+          }, { 
+          role: "user", 
+          content: userMessage
+        }],
+    });
+    const message = chatCompletion.data.choices[0].message
+    return message
+  } catch(error: any) {
+    console.log(error.response.data)
+  }
+}
 
 
 
