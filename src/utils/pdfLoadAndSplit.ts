e270a8aad305a757cda12870a6824963d20a9829
linghong <linghong_chen@hotmail.com>
2023-07-11 20:10:41 -0400

    Split loadAndSplit function into two functions for better maintainability

--- src/utils/pdfLoadAndSplit.ts
+++ src/utils/pdfLoadAndSplit.ts
@@ -2,35 +2,48 @@
  * This PDF Loader and Splitter is designed to work with converted PDFs and is not compatible with PDFs generated from scanned documents. 
  * Scanned documents will require separate handling due to their unique conditions.
  *
- * The Langchain PDFLoader used here, which utilizes the PDF loading functionality from the pdf.js library, appends a newline character at the end of each line during text extraction 
+ * The Langchain PDFLoader used here appends a newline character at the end of each line during text extraction,  
  * However, newline characters (\n) often do not coincide with the end of a sentence. 
- * The goal of this code is to mitigate this issue, though it may not entirely eliminate all related problems.
+ * The goal of the regExpress code here is to mitigate this issue, though it may not entirely eliminate all related problems.
  *
- * Due to the complexity and variability of PDF file structures, particularly those with embedded tables, the current implementation is primarily targeted at handling non-tabular English-language text for  a significantly better outcome than the original Langchain code.
+ * Due to the complexity and variability of PDF file structures, particularly those with embedded tables, the current implementation is primarily targeted at handling non-tabular English-language text for a significantly better outcome than the original Langchain method.
  */
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
-const loadAndSplit  = async ( docPath :string)  => {
+type Document = {
+  pageContent: string,
+  metadata: Record<string, any>
+};
+
+const joinBrokenSentence = (pageContent: string): string => {
+  // Handle words that got split between lines, or two words that are combined into one.
+  let text = pageContent.replace(/-\n/g, '').replace(/\n-/g, '-')
+
+  // Replaces newline characters that are part of numbered lists.
+  text = text.replace(/(?<=^\b[0-9a-zA-Z]{1}\.|•)\n/gm, ' ')
+
+  // Replace newline characters that less likely represent sentence boundaries. 
+  //This occurs when both the preceding characters are not typical sentence endings and the following characters do not typically start a sentence.
+  //The '?' character is excluded from this process to prevent splitting potential answers.
+  text = text.replace(/(?<![.!] *\)*|[.!]") *\n(?= *[a-z0-9!?:;,.@%&$ ])/g, ' ')
+
+  return text
+}
+
+const loadAndSplit  = async ( docPath: string) : Promise<Document[]> => {
+
+  const extension = docPath.split('.').pop()?.toLowerCase()
+  if (extension !== 'pdf') throw new Error('File is not a PDF')
+
   try {
-    const loader = new PDFLoader(docPath,  { splitPages: false})
+    const loader = new PDFLoader(docPath, { splitPages: false})
     const document = await loader.load()
         
-    const metadata = document[0].metadata
+    const metadata : Record<string, any> = await document[0].metadata
 
-    // Handle words that got split between lines, or two words that are combined into one.
-    const text1 = document[0].pageContent.replace(/-\n/g, '').replace(/\n-/g, '-')
-
-    // When a period exists before a newline character (\n), it could be part of a numbered list (like 1.Item1, 2.Item2, etc.).
-    // In such cases, we want to replace the newline character with a space, effectively joining the split items onto the same line.
-    const text2 = text1.replace(/(?<=^\b[0-9a-zA-Z]{1}\.|•)\n/gm, ' ')
-
-    // when a line doesn't end with .(including ...), ! (including ?!), .", !", .), !) or !") or ."), it is less likely to be the end of a sentence.
-    // As we may want the answer, we avoid moving it to a newline. Therefore, '?' is not included in the above sentence-ending conditions.
-    // If such a line is followed by a character that is more likely to be connected to the previous sentence,
-    // remove the newline ("\n") and replace it with a space (" ").
-    const text = text2.replace(/(?<![.!] *\)*|[.!]") *\n(?= *[a-z0-9!?:;,.@%&$ ])/g, ' ')
-
+    const preparedText = joinBrokenSentence(document[0].pageContent)
+  
     // The regex used as separators serves the following purposes:
     // 1.'(?<![.!] *\)*|[.!]")\n': split when newline characters that are more likely to be at the end of a sentence. 
     // 2.'\n(?![?)}]|\])': split when newline characters not followed by a question mark and closing brackets - these are unlikely to be at the end of sentences.
@@ -42,12 +55,12 @@ const loadAndSplit  = async ( docPath :string)  => {
       separators: ['\n\n','(?<![.!] *\)*|[.!]")\n', '\n(?![?)}]|\])', '(?<![.!] *|[.!]"|[.!] *) ', ' ', '']
     })
 
-    const documents = await splitter.createDocuments([text], [metadata])
+    const documents: Document[] = await splitter.createDocuments([preparedText], [metadata])
+
     return documents
-    
   } catch (e) {
-    console.error("Failed to load or split Document.", e)
-    throw new Error("Failed to load or split Document.")
+    console.error(e)
+    throw new Error(`Failed to load or split document: ${e}`)
   }
 }
 
