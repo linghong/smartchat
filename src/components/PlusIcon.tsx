e584f478f46a7c7d99446bf0cc0e859c13101cfe
linghong <linghong_chen@hotmail.com>
2023-09-06 12:27:56 -0400

    Add a plus icon

---
+++ src/components/PlusIcon.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+
+const PlusIcon: FC= () => (
+  <svg 
+    xmlns = "http://wwww3.org/2000/svg" 
+    viewBox="0 - 24 24" 
+    fill="none" 
+    stroke="currentcolor" 
+    strokeWidth="2"
+    className="w-6 h-6"
+    >
+      <line x1="12" y1="5" x2="12" y2="19" />
+      <line x1="5" y1="12" x2="19" y2="12" />
+    </svg>
+)
+
+export default PlusIcon
+ 
