94d1306df3761beaa14142a0133c16f543ea8f58
linghong <linghong_chen@hotmail.com>
2023-09-06 19:33:18 -0400

    Add input area for PDF  uploads

--- src/pages/managemyai.tsx
+++ src/pages/managemyai.tsx
@@ -1,11 +1,19 @@
-import { FC } from 'react'
+import { FC, ChangeEvent } from 'react'
 import Header from '@/src/components/Header'
 
 const UploadFile : FC = () => {
+  const handlePdfUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.files && e.target.files[0]){
+      const file = e.target.files[0]
+      console.log('pdf Upload')
+    }
+  }
+  
   return (
     <div className="flex flex-col">
       <Header pageTitle="Manage My AI" />
       <div className="flex flex-col h-80vh items-center justify-between">
+        <input type="file" accept=".pdf" onChange={handlePdfUpload} />
       </div>
     </div>
   )
