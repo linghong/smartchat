145fb323570843cfc406b4f7f9fc059773be6a23
linghong <linghong_chen@hotmail.com>
2023-09-09 23:44:38 -0400

    Add front end code to submit the uploaded file tothe  server

--- src/pages/managemyai.tsx
+++ src/pages/managemyai.tsx
@@ -1,20 +1,66 @@
-import { FC, ChangeEvent } from 'react'
+import { FC, ChangeEvent, MouseEvent, useState } from 'react'
 import Header from '@/src/components/Header'
 
+type ApiResponse = {
+  message: string;
+  fileName: string;
+  error?: string;
+}
+
 const UploadFile : FC = () => {
-  const handlePdfUpload = async (e: ChangeEvent<HTMLInputElement>) => {
-    if(e.target.files && e.target.files[0]){
-      const file = e.target.files[0]
-      console.log('pdf Upload')
+  const [selectedFile, setSelectedFile] =useState<File |null>(null)
+
+  const handleFileChange= (e: ChangeEvent<HTMLInputElement>) =>{
+    const files = e.target.files
+    if(files && files[0]){
+      setSelectedFile(files[0])
     }
   }
-  
+
+  const handlePdfUpload = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    if(selectedFile){
+      const formData = new FormData()
+      formData.append('file', selectedFile)
+
+      try {
+        const res = await fetch('/api/upload', {
+          method: 'POST',
+          body: formData,
+        })
+
+        const data: ApiResponse = await res.json() as ApiResponse
+
+        if (res.ok) {
+          console.log('Upload success:', data);
+        } else {
+          console.error('Upload error:', data.error);
+        }
+
+      } catch (error) {
+        console.error('There was a network error when sending file:', error)
+      }
+    }
+  }
+
   return (
     <div className="flex flex-col">
       <Header pageTitle="Manage My AI" />
-      <div className="flex flex-col h-80vh items-center justify-between">
-        <input type="file" accept=".pdf" onChange={handlePdfUpload} />
-      </div>
+      <form className="flex flex-col h-80vh items-center justify-between">
+        <input 
+          type="file" 
+          accept=".pdf" 
+          onChange={handleFileChange}
+        />
+        <button
+          type="button"
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" 
+          onClick={handlePdfUpload}
+        >
+          Upload
+        </button>
+      </form>      
     </div>
   )
 }
