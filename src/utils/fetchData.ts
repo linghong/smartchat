2134bc23739ed02f8cfdf57774ca7c43c7ad7fca
linghong <linghong_chen@hotmail.com>
2023-09-16 17:24:37 -0400

    Add a function to fetch data from API endpoint

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,22 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+export const fetchData = async (endpoint: string) => {
+  try {
+    const response = await fetch(`${API_URL}/api/${endpoint}`);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching ${endpoint}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error(`Error fetching ${endpoint}:`, error);
+    return {
+      props: {
+        error: "Failed to fetch namespaces."
+      }
+    };
+  }
+};
