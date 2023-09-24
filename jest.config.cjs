a30ec13521f1db243f1cee69ad0829992aa15d33
linghong <linghong_chen@hotmail.com>
2023-09-23 20:47:59 -0400

    Set up jest unit testing

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/src/(.*)$': '<rootDir>/src/$1',
+  }
+}
