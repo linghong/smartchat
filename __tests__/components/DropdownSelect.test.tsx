697893e55e0eb15d3003d4b6b9ee0d60eff48017
linghong <linghong_chen@hotmail.com>
2023-09-25 15:43:51 -0400

    Add unit tests for DropdownSelect component

---
+++ __tests__/components/DropdownSelect.test.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { render, screen, act } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
+import DropdownSelect from '@/src/components/DropdownSelect'
+
+const options = [
+  { value: 'option1', label: 'Option 1' },
+  { value: 'option2', label: 'Option 2' },
+]
+
+const label = 'Dropdown Label'
+const name = 'dropdownName'
+
+describe('DropdownSelect Component', () => {
+  let mockChangeHandler : jest.Mock
+
+  beforeEach(() => {
+    mockChangeHandler = jest.fn()
+    render(<DropdownSelect 
+      selectedOption={options[0]} 
+      onChange={mockChangeHandler} 
+      options={options} 
+      label={label}
+      name={name}
+    />)
+  })
+
+  it('renders DropdownSelect component correctly', () => {
+    // Check if label is in the document
+    expect(screen.getByText(label)).toBeInTheDocument()
+    
+    // Check if the dropdown is rendered with the correct selected option
+    expect(screen.getByText(options[0].label)).toBeInTheDocument()
+  })
+
+  it('handles option selection', async () => {
+    await act( async () => {
+      await userEvent.click(screen.getByText(options[0].label))
+    })
+    await act( async () => {
+      await userEvent.click(screen.getByText(options[1].label));
+   });
+
+    // Check if the change handler is called with the correct option
+    expect(mockChangeHandler).toHaveBeenCalledWith(options[1], expect.any(Object))
+  })
+
+  it('applies custom styles when selected', () => {
+      const optionElement = screen.getByText(options[0].label);
+      const originalStyles = window.getComputedStyle(optionElement)
+
+      // Fire a click event to select the element
+      userEvent.click(optionElement);
+
+      const appliedStyles = window.getComputedStyle(optionElement);
+      expect(appliedStyles).not.toBe(originalStyles)
+  })
+
+  it('applies custom styles on focus', async () => {
+    const optionElement = screen.getByText(options[0].label)
+    const originalStyles = window.getComputedStyle(optionElement)
+
+    // Fire a hover or focus event
+    userEvent.hover(optionElement)
+  
+    const appliedStyles = window.getComputedStyle(optionElement)
+
+    expect(appliedStyles).not.toBe(originalStyles)
+  })
+
+  it('displays Dropdown component snapshot', () => {
+    const { asFragment } = render(<DropdownSelect 
+      selectedOption={options[0]} 
+      onChange={mockChangeHandler} 
+      options={options} 
+      label={label}
+      name={name}
+    />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
