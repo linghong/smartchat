0f07859d991f80ebf7a4608927c51e732d241788
linghong <linghong_chen@hotmail.com>
2024-05-21 12:39:30 -0400

    Add test for fetchData.ts file

---
+++ __tests__/utils/fetchData.test.ts
@@ -0,0 +1,61 @@
+// __tests__/utils/fetchData.test.ts
+import { fetchData } from '@/src/utils/fetchData'
+import { enableFetchMocks } from 'jest-fetch-mock'
+
+// Enable fetch mocks
+enableFetchMocks()
+
+describe('fetchData', () => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+  beforeEach(() => {
+    // Resets the state of all fetch mocks provided by the jest-fetch-mock library
+    fetch.resetMocks()
+    
+    // Mocks console.error to prevent actual error logs during tests and allows verification of error logs
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    // Restores all mocks created by jest.spyOn, jest.mock, and jest.fn to their original implementations
+    jest.restoreAllMocks() 
+  })
+
+  it('should fetch data successfully', async () => {
+    const mockData = { key: 'value' }
+    fetch.mockResponseOnce(JSON.stringify(mockData))
+
+    const data = await fetchData('test-endpoint')
+
+    expect(fetch).toHaveBeenCalledWith(`${API_URL}/api/test-endpoint`)
+    expect(data).toEqual(mockData)
+  })
+
+  it('should handle fetch error', async () => {
+    fetch.mockRejectOnce(new Error('Network Error'))
+
+    const data = await fetchData('test-endpoint')
+
+    expect(fetch).toHaveBeenCalledWith(`${API_URL}/api/test-endpoint`)
+    expect(console.error).toHaveBeenCalledWith('Error fetching test-endpoint:', expect.any(Error))
+    expect(data).toEqual({
+      props: {
+        error: 'Failed to fetch data.'
+      }
+    })
+  })
+
+  it('should handle non-OK response1', async () => {
+    fetch.mockResponseOnce('', { status: 500, statusText: 'Internal Server Error' });
+
+    const data = await fetchData('test-endpoint');
+
+    expect(fetch).toHaveBeenCalledWith(`${API_URL}/api/test-endpoint`);
+    expect(console.error).toHaveBeenCalledWith('Error fetching test-endpoint:', new Error('Error fetching test-endpoint: Internal Server Error'));
+    expect(data).toEqual({
+      props: {
+        error: 'Failed to fetch data.'
+      }
+    });
+  });
+})
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -2,21 +2,21 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const fetchData = async (endpoint: string) => {
   try {
-    const response = await fetch(`${API_URL}/api/${endpoint}`);
+    const response = await fetch(`${API_URL}/api/${endpoint}`)
 
     if (!response.ok) {
-      throw new Error(`Error fetching ${endpoint}: ${response.statusText}`);
+      throw new Error(`Error fetching ${endpoint}: ${response.statusText}`)
     }
 
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
 
   } catch (error) {
-    console.error(`Error fetching ${endpoint}:`, error);
+    console.error(`Error fetching ${endpoint}:`, error)
     return {
       props: {
-        error: "Failed to fetch namespaces."
+        error: "Failed to fetch data."
       }
-    };
+    }
   }
-};
+}
