f28f783d9a6e94741d5c0869347908e745521c77
linghong <linghong_chen@hotmail.com>
2024-05-26 14:48:37 -0400

    Add Keydown feature to the image upload icon

--- src/components/ImageUploadIcon.tsx
+++ src/components/ImageUploadIcon.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, ChangeEvent, FC } from 'react'
 import { BsImageFill  } from "react-icons/bs"
-
 interface ImageUploadIconProps {
   onImageUpload: (file: File) => void
 }
@@ -8,28 +7,37 @@ interface ImageUploadIconProps {
 const ImageUploadIcon: FC<ImageUploadIconProps> = ({ onImageUpload }) => {
   const fileInputRef = useRef<HTMLInputElement>(null)
 
+  // Trigger the file input when onKeyDown is fired
   const handleIconClick = () => {
-    fileInputRef.current?.click() 
-}
+    fileInputRef.current?.click()
+  };
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files ? event.target.files[0] : null
-    console.log("file", file)
+    const file = event.target.files ? event.target.files[0] : null 
     if (file) {
       onImageUpload(file)
     }
-  };
+  }
 
-  return (
-    <label className="flex items-center justify-center  text-white font-bold py-2 px-2 rounded cursor-pointer">
+  return ( 
+    <label 
+      className="flex items-center justify-center  text-white font-bold py-2 px-2 rounded cursor-pointer"
+      tabIndex={0}
+      onKeyDown={(e) => e.key === 'Enter' && handleIconClick()}
+    >
       <input 
         type="file" 
         ref={fileInputRef}
         onChange={handleFileChange} 
         style={{ display: 'none' }} 
-        accept="image/*"        
+        accept="image/*"
+        aria-label="Upload image"       
       />
-      <BsImageFill size={24} onClick={handleIconClick} style={{ cursor: 'pointer', background: '#7b7bdc', border:'solid black 1px' }} />
+      <BsImageFill 
+        size={24} 
+        style={{ cursor: 'pointer', background: '#7b7bdc', border:'solid black 1px' }}
+        aria-hidden="true" // Hides the icon from screen readers
+      />
     </label>
   )
 }
