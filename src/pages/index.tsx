1f3cbf240d0dc5692c6c2704b2c75ce2bf384858
linghong <linghong_chen@hotmail.com>
2024-06-03 12:10:45 -0400

    move modelOptions to config/modellist.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {useState, useRef, useCallback, useEffect, ChangeEvent,FormEvent, FC } from 'react'
 import {GetStaticProps} from 'next'
 
-import { fetchData } from '@/src/utils/fetchData'
+import { modelOptions } from '@/config/modellist'
 import ArrowButton  from '@/src/components/ArrowButton'
 import ChatMessage from '@/src/components/ChatMessage'
 import DropdownSelect from '@/src/components/DropdownSelect'
@@ -11,21 +11,8 @@ import ImageUploadIcon from '@/src/components/ImageUploadIcon'
 import Notification from '@/src/components/Notification'
 import { Message } from '@/src/types/chat'
 import { OptionType } from '@/src/types/common'
+import { fetchData } from '@/src/utils/fetchData'
 
-const modelOptions: OptionType[] = [
-  { value: 'gpt-4o', label: 'GPT-4o', category: 'openai', contextWindow: 128000 },
-  { value: 'gpt-4-turbo', label: 'GPT-4 Turbo', category: 'openai', contextWindow: 128000 },
-  { value: 'gpt-3.5-turbo', label: 'GPT-3.5', category: 'openai', contextWindow: 16385 },
-  { value: 'gpt-4', label: 'GPT-4', category: 'openai', contextWindow: 8192 },
-  { value: 'gemini-1.5-pro', label: 'Gemini-1.5 Pro', category: 'google', contextWindow: 128000 },
-  { value: 'gemini-1.5-flash', label: 'Gemini-1.5 Flash', category: 'google', contextWindow: 128000 },
-  { value: 'meta-llama/Llama-2-7b-chat-hf', label: 'Llama-2-7b-chat-hf', category:'hf-large', contextWindow: 4000 }, 
-  { value: 'microsoft/phi-1_5', label: 'phi-1_5', category: 'hf-small', contextWindow:1000},
-  { value: 'llama3-8b-8192', label: 'LLaMA3 8b',  category:'groq', contextWindow: 8192 },
-  { value: 'llama3-70b-8192', label: 'LLaMA3 70b',  category:'groq', contextWindow: 8192 },
-  { value: 'mixtral-8x7b-32768', label: 'Mixtral 8x7b',  category:'groq', contextWindow: 32768 },
-  { value: 'gemma-7b-it', label: 'Gemma 7b',  category:'groq', contextWindow: 8192 }
-]
 
 const initialFileCategory: OptionType = {value: 'none', label: 'None'}
 
