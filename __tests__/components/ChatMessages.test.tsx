166bcd9b8fcceba5bcefe4a12a5877a64e606699
linghong <linghong_chen@hotmail.com>
2024-06-03 22:28:20 -0400

    Update ChatMessages.tsx unit tests

--- __tests__/components/ChatMessages.test.tsx
+++ __tests__/components/ChatMessages.test.tsx
@@ -8,17 +8,28 @@ describe('ChatMessage Component', () => {
   let message : Message
   let loading : boolean
   let lastIndex : boolean
+  let imageSrc: string[]
+  let handleImageDelete: jest.Mock
 
   beforeEach(() => {
     message = { question: 'What is AI?', answer: 'AI stands for Artificial Intelligence.' }
     loading = false
     lastIndex = true
-    render(<ChatMessage message={message} lastIndex={lastIndex} loading={loading} />)
+    handleImageDelete = jest.fn()
+    imageSrc = ['/path/to/image1.png', 'path/to/image2.png']
+    render(<ChatMessage 
+      message={message} 
+      lastIndex={lastIndex} 
+      loading={loading} 
+      imageSrc={imageSrc}
+      handleImageDelete={handleImageDelete}
+      />)
   })
 
   afterEach(() => {
     jest.clearAllMocks()
-  });
+  })
+
   it('renders ChatMessage component correctly with question and answer', () => {
     expect(screen.getByText('What is AI?')).toBeInTheDocument()
     expect(screen.getByText('AI stands for Artificial Intelligence.')).toBeInTheDocument()
@@ -26,23 +37,41 @@ describe('ChatMessage Component', () => {
 
   it('applies animate-pulse class when loading and is the last index', () => {
     act (() => {
-      render(<ChatMessage message={message} lastIndex={true} loading={true} />)
+      render(<ChatMessage 
+        message={message} 
+        lastIndex={true} 
+        loading={true}
+        imageSrc={imageSrc}
+        handleImageDelete={handleImageDelete}
+      />)
     })
-    const botImages = screen.getAllByAltText('AI Bot') as HTMLElement[]
+    const botImages = screen.getAllByAltText('AI bot avatar') as HTMLElement[]
     expect(botImages[botImages.length - 1]).toHaveClass('animate-pulse')
   })
 
   it('does not apply animate-pulse class when not loading', () => {
     act (() => {
-      render(<ChatMessage message={message} lastIndex={true} loading={false} />)
+      render(<ChatMessage 
+        message={message} 
+        lastIndex={true} 
+        loading={false}
+        imageSrc={imageSrc}
+        handleImageDelete={handleImageDelete}
+      />)
     })
    
-    const botImages = screen.getAllByAltText('AI Bot') as HTMLElement[]
+    const botImages = screen.getAllByAltText('AI bot avatar') as HTMLElement[]
     expect(botImages[botImages.length - 1]).not.toHaveClass('animate-pulse')
   })
   
   it('ChatMessage component snapshot', () => {
-    const { asFragment } =  render(<ChatMessage message={message} lastIndex={lastIndex} loading={loading} />)
+    const { asFragment } =  render(<ChatMessage 
+      message={message} 
+      lastIndex={lastIndex} 
+      loading={loading}
+      imageSrc={imageSrc}
+      handleImageDelete={handleImageDelete}
+    />)
     expect(asFragment()).toMatchSnapshot()
   })
 })
