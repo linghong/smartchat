099ef8337e9e236fdd0910554182ae6697981ea4
linghong <linghong_chen@hotmail.com>
2024-06-10 23:33:30 -0400

    update tests for Sidebar.test.tsx

--- __tests__/components/Sidebar.test.tsx
+++ __tests__/components/Sidebar.test.tsx
@@ -1,39 +1,36 @@
 import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
-import { act } from 'react-dom/test-utils'
+import { render,  screen } from '@testing-library/react'
+
 import Sidebar from '@/src/components/Sidebar'
 import { useRouter } from 'next/router'
-
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
 }))
 const mockPush = jest.fn()
 
-
 describe('Sidebar Component', () => {
+  const setIsSidebarOpen = jest.fn()
+  const messageSubjectList = ['Test Subject']
 
   beforeEach(() => {
-    // reset the state of all mocks to to their initial state (clears call history, 
-    // instances, return values, and implementations)
+    // Reset the state of all mocks to their initial state
     jest.resetAllMocks()
 
-    // Must apply the type assertion before attempting to call any methods
-    // Chaining method calls directly on type assertions can produce errors
-    // (useRouter as jest.Mock).mockReturnValue({ push: mockPush })  --error
     const mockedUseRouter = useRouter as jest.Mock
 
     // Set the mock to return a specified value telling the mock that whenever useRouter() is called
     // it should return an object with a push method (mockPush)
     mockedUseRouter.mockReturnValue({ push: mockPush })
-
-    act(() => {
-      render(<Sidebar />)
-    })
+    render(
+      <Sidebar
+        setIsSidebarOpen={setIsSidebarOpen}
+        messageSubjectList={messageSubjectList}
+      />
+    )
   })
-
+ 
   test('renders Sidebar component with all expected elements', () => {
-    expect(screen.getByText('New Chat')).toBeInTheDocument()
     expect(screen.getByText('Manage My AI')).toBeInTheDocument()
     expect(screen.getByText('Finetune AI Model')).toBeInTheDocument()
     expect(screen.getByText('Chat with AI')).toBeInTheDocument()
@@ -42,26 +39,7 @@ describe('Sidebar Component', () => {
   test('initially renders child menu items correctly based on defaultOpen state', () => {
     expect(screen.queryByText('File1')).toBeNull()
     expect(screen.queryByText('My Model1')).toBeNull()
-    expect(screen.getByText('Chat 1')).toBeInTheDocument() // Visible because defaultOpen is true
-  })
-
-  test('Clicking onNewChat button navigate to the home page', async () => {
-    await act(async () => {
-      const newChatDiv = document.querySelector('#newChatDiv')
-      fireEvent.click(newChatDiv)
-    })
-    expect(mockPush).toHaveBeenCalledTimes(1)
-    expect(mockPush).toHaveBeenCalledWith('/')
-  })
-
-  test('hovering over New Chat should show visual feedback', async () => {
-    const newChatButton = document.querySelector('#newChat')
-    expect(newChatButton.className).toMatch(/hover/)
-  })
-
-  test('focusing on New Chat should change its background color', async () => {
-    const newChatButton = document.querySelector('#newChat')
-    expect(newChatButton.className).toMatch(/focus/)
+    expect(screen.getByText('Test Subject')).toBeInTheDocument() // Visible because defaultOpen is true
   })
 
   test('Each menuItem has the correct href', () => {
@@ -76,9 +54,12 @@ describe('Sidebar Component', () => {
   })
 
   test('Sidebar component matches snapshot', () => {
-    act(() => {
-      const { asFragment } = render(<Sidebar />)
-      expect(asFragment()).toMatchSnapshot()
-    })
+    const { asFragment } = render(
+      <Sidebar
+        setIsSidebarOpen={setIsSidebarOpen}
+        messageSubjectList={messageSubjectList}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 })
